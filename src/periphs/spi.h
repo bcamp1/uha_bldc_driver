51ea20fe0e247f735e24412f9fe80b05412a24a1
Branson Camp <brans@embedded-workstation.home.local>
2025-08-19 18:28:18 -0600

    Removed multi-motor configs

--- src/periphs/spi.h
+++ src/periphs/spi.h
@@ -31,12 +31,8 @@ typedef struct {
 	uint8_t cs;
 } SPIConfig;
 
-extern const SPIConfig SPI_CONF_MTR_DRVR_A;
-extern const SPIConfig SPI_CONF_MTR_DRVR_B;
-extern const SPIConfig SPI_CONF_TENSION_ARM_A;
-extern const SPIConfig SPI_CONF_TENSION_ARM_B;
-extern const SPIConfig SPI_CONF_MTR_ENCODER_A;
-extern const SPIConfig SPI_CONF_MTR_ENCODER_B;
+extern const SPIConfig SPI_CONF_MTR_DRVR;
+extern const SPIConfig SPI_CONF_MTR_ENCODER;
 
 void spi_init(const SPIConfig* inst);
 uint16_t spi_write_read16(const SPIConfig* inst, uint16_t data);
